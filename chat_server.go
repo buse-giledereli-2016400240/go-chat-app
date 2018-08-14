9a78b8ae614cc5c2bb37a53427f7f66f9efb2b02
buse-giledereli-2016400240 <buse.giledereli@boun.edu.tr>
2018-08-14 16:12:32 +0300

    updated

--- chat_server.go
+++ chat_server.go
@@ -1,83 +1,53 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
 	"log"
-	"net"
+	"net/http"
+
+	"github.com/googollee/go-socket.io"
 )
 
-func main() {
-	//holds a map of connections mainly used to broadcast received messages to every connection
-	connections := make(map[net.Conn]string)
-	//holds a channel of connection requests
-	addNewConnection := make(chan net.Conn)
-	//holds a channel of messages to be broadcasted
-	postMsg := make(chan string)
-	//opens a listener for port 31415
-	listener, err := net.Listen("tcp", ":31415")
-	if err != nil {
-		panic(err)
-	}
-	//Goroutine running an infinite loop to add new connection requests to addNewConnections channel
-	go func() {
-		for {
-			connection, err := listener.Accept()
-			if err != nil {
-				panic(err)
-			}
-			addNewConnection <- connection
-		}
-	}()
+// Message object
+type Message struct {
+	Username string `json:"username"`
+	Message  string `json:"message"`
+	Color    string `json:"color"`
+}
 
-	//Infinite loop to select one of the channels and either add a new client or broadcast a message
-	for {
-		select {
-		//adds a new client and receives messages
-		case conn := <-addNewConnection:
-			connReader := bufio.NewReader(conn)
-			//Prompts the client to choose a username to broadcast the messages in a more organised way
-			conn.Write([]byte("Choose username: "))
-			username, tooLong, err := connReader.ReadLine()
-			if tooLong {
-				conn.Write([]byte("Too long, shortened version is set."))
-			}
-			if err != nil {
-				conn.Close()
-				break
-			}
-			log.Printf("Accepted new client, %s", username)
-			connections[conn] = string(username)
-			//Goroutine running an infinite loop to add new messages to the broadcast channel
-			go func(conn net.Conn, username string) {
-				for {
-					message, err := connReader.ReadString('\n')
-					if err != nil {
-						//if there is an error connecting, the user disconnected so it is treated accordingly
-						conn.Close()
-						log.Printf("%s disconnected", username)
-						delete(connections, conn)
-						break
-					} else {
-						//message is sent to the postMsg, broadcasting channel
-						log.Printf("Message by %s", username)
-						postMsg <- fmt.Sprintf("%s : %s", username, message)
-					}
-				}
-			}(conn, connections[conn])
-		//Messages on the channel are broadcasted to all clients
-		case msg := <-postMsg:
-			for conn := range connections {
-				//Goroutine writing the message from the channel to all clients
-				go func(conn net.Conn) {
-					_, err := conn.Write([]byte(msg + "\n"))
-					if err != nil {
-						conn.Close()
-						log.Printf("%s disconnected", connections[conn])
-						delete(connections, conn)
-					}
-				}(conn)
-			}
-		}
+var usernameMap = make(map[string]string)
+
+func handleConnections(so socketio.Socket) {
+
+	so.Join("default-chat-room")
+	so.On("joined", func(msg Message) {
+		log.Println("A new user connected")
+		usernameMap[so.Id()] = msg.Username
+		so.Emit("joined", msg)
+		so.BroadcastTo("default-chat-room", "joined", msg)
+	})
+	so.On("message-sent", func(msg Message) {
+		log.Println("A new message sent")
+		so.Emit("message-sent", msg)
+		so.BroadcastTo("default-chat-room", "message-sent", msg)
+	})
+	so.On("disconnection", func() {
+		discUser := usernameMap[so.Id()]
+		so.Emit("userDisconnected", discUser)
+		so.BroadcastTo("default-chat-room", "userDisconnected", discUser)
+	})
+}
+
+func main() {
+
+	server, err := socketio.NewServer(nil)
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	server.On("connection", handleConnections)
+
+	http.Handle("/socket.io/", server)
+	http.Handle("/", http.FileServer(http.Dir("./client")))
+	log.Println("Server on port 9000")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
