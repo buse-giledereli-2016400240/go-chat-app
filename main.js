0e1c59ba76f4d74278f639d89645b56daccfe2c0
buse-giledereli-2016400240 <buse.giledereli@boun.edu.tr>
2018-09-27 16:04:36 +0300

    appendice 6&7

--- main.js
+++ main.js
@@ -6,18 +6,15 @@ var random_color = colors[Math.floor(Math.random() * colors.length)];
 new Vue({
     el: '#chat-app',
     data: {
-        newMsg:'', //holds new messages to be sent to the server
-        messageList: [], //running list of chat messages displayed on the screen
-        username: '', //our username
-        password: '', //our password
-        userColor: random_color, //our randomly chosen color
-        usersList: [], //running list of usernames displayed on the screen
-        joined: false, // True username have been filled in
-        entertypechosen: false, //will be true if we click sign up or log in
-        loginchosen: false //will be true if we click log in
+        newMsg:'', // Holds new messages to be sent to the server
+        messageList: [], // A running list of chat messages displayed on the screen
+        username: '', // Our username
+        userColor: random_color,
+        usersList: [],
+        joined: false // True if email and username have been filled in
     },
     created() {
-        socket.on("messageSent", (msg) => {
+        socket.on("message-sent", (msg) => {
             currDate = new Date();
             this.messageList.push({
                 username: msg.username,
@@ -40,22 +37,7 @@ new Vue({
                 username: msg.username,
             })
             var element = document.getElementById('chat-messages');
-            element.scrollTop = element.scrollHeight+10; //auto scroll to the bottom
-        })
-        socket.on("joinedSuccessfully", (msg) => {
-            this.joined = true;
-            currDate = new Date();
-            this.messageList.push({
-                username: msg.username,
-                userColor: msg.color,
-                text: 'joined',
-                date: currDate.getHours() + ":" + currDate.getMinutes()
-            })
-            this.usersList.push({
-                username: msg.username,
-            })
-            var element = document.getElementById('chat-messages');
-            element.scrollTop = element.scrollHeight+10; //auto scroll to the bottom
+            element.scrollTop = element.scrollHeight+10; // Auto scroll to the bottom
         })
         socket.on("userDisconnected", (uName) => {
             currDate = new Date();
@@ -70,35 +52,29 @@ new Vue({
             })
             
             var element = document.getElementById('chat-messages');
-            element.scrollTop = element.scrollHeight+10; //auto scroll to the bottom
+            element.scrollTop = element.scrollHeight+10; // Auto scroll to the bottom
         })
         socket.on("receiveUserList", (uName) => {
             this.usersList.push({
                 username: uName
             })
         })
-        socket.on("errorJoining", (errorDesc) => {
-            alert(errorDesc)
-        })
     },
     methods: {
         sendMessage () {
             if (this.newMsg != '') {
-                socket.emit("messageSent", {username: this.username, password: this.password, message: this.newMsg, color: this.userColor});
+                socket.emit("message-sent", {username: this.username, message: this.newMsg, color: this.userColor});
                 this.newMsg = ''; 
             }else {
                 alert("You haven't typed a message!");
             }
         },
         join () {
-            if(this.username == '' || this.password == '') {
-                alert("Username and password required!");
+            if(this.username != '') {
+                socket.emit("joined", {username: this.username, message: this.newMsg, color: this.userColor});
+                this.joined = true;
             }else {
-                if (this.loginchosen){
-                    socket.emit("logInRequest", {username: this.username, password: this.password, message: this.newMsg, color: this.userColor});
-                }else{
-                    socket.emit("signUpRequest", {username: this.username, password: this.password, message: this.newMsg, color: this.userColor});
-                }
+                alert("Username required!");
             }
         },
     }
